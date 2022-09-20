1e78d478d1ae3a2ca8ce5c7bee101d7b962a6117
ElisonMartins <martinselison10@gmail.com>
2022-09-20 19:18:30 -0300

    1.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TodoList from './components/TodoList.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
